6f3ce75bd5b76f2b84c469a588a53499709e1419
vzcatus13 <vzcatus13@protonmail.ch>
2021-08-28 19:57:40 +0300

    Banner title bug fix.

--- src/pages/Title/Banner/Banner.js
+++ src/pages/Title/Banner/Banner.js
@@ -2,14 +2,22 @@ import Skeleton from "../../../components/Skeleton";
 import { BannerWrapper } from "./Banner.styled";
 
 const Banner = ({ data, loading }) => {
+  let titles, title;
+
   if (!loading) {
     const imgSrc = data.Media.bannerImage
       ? data.Media.bannerImage
       : data.Media.coverImage.extraLarge;
 
+    titles = Object.entries(data.Media.title)
+      .filter((element) => element[0] !== "__typename")
+      .filter((element) => element[1] !== null)
+      .map((element) => element[1]);
+    title = titles.shift();
+
     return (
       <BannerWrapper blur={!data.Media.bannerImage}>
-        <img src={imgSrc} alt={data.Media.title.english} />
+        <img src={imgSrc} alt={title} />
       </BannerWrapper>
     );
   }
