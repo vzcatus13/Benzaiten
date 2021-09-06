b175c975b3899a65b18a92b2f6b090c2e55d5cd0
vzcatus13 <vzcatus13@protonmail.ch>
2021-09-06 16:43:57 +0300

    Added Scrollable Container ref forwading

--- src/components/ScrollableCarousel/ScrollableCarousel.js
+++ src/components/ScrollableCarousel/ScrollableCarousel.js
@@ -1,5 +1,5 @@
 import { NavigateBefore, NavigateNext } from "@material-ui/icons";
-import { useEffect, useRef, useState } from "react";
+import { forwardRef, useEffect, useRef, useState } from "react";
 
 import {
   ScrollableWrapper,
@@ -8,165 +8,176 @@ import {
   ScrollableContainer,
 } from "./ScrollableCarousel.styled";
 
-const ScrollableCarousel = ({
-  children,
-  buttonSize,
-  buttonColor,
-  buttonHoverColor,
-  buttonBackgroundColor,
-  buttonShadowColor,
-  buttonZIndex,
-  hideButtons,
-  disableScroll,
-  onScroll,
-  CustomButtonLeft,
-  CustomButtonRight,
-}) => {
-  const scrollableContainerRef = useRef();
+const ScrollableCarousel = forwardRef(
+  (
+    {
+      children,
+      buttonSize,
+      buttonColor,
+      buttonHoverColor,
+      buttonBackgroundColor,
+      buttonShadowColor,
+      buttonZIndex,
+      hideButtons,
+      disableScroll,
+      onScroll,
+      CustomButtonLeft,
+      CustomButtonRight,
+    },
+    ref
+  ) => {
+    const scrollableContainerRef = useRef();
 
-  const [dimensions, setDimensions] = useState({
-    width: 0,
-    scrollWidth: 0,
-    height: 0,
-  });
+    const [dimensions, setDimensions] = useState({
+      width: 0,
+      scrollWidth: 0,
+      height: 0,
+    });
 
-  const [leftButtonVisibility, setLeftButtonVisibility] = useState(false);
-  const [rightButtonVisibility, setRightButtonVisibility] = useState(true);
+    const [leftButtonVisibility, setLeftButtonVisibility] = useState(false);
+    const [rightButtonVisibility, setRightButtonVisibility] = useState(true);
 
-  const isEnd = (width, scrollWidth, scrollLeft) =>
-    Math.round(scrollWidth) - Math.round(scrollLeft) <= Math.round(width);
+    const isEnd = (width, scrollWidth, scrollLeft) =>
+      Math.round(scrollWidth) - Math.round(scrollLeft) <= Math.round(width);
 
-  useEffect(() => {
-    const ref = scrollableContainerRef.current;
-    if (ref) {
-      setDimensions({
-        width: ref.offsetWidth,
-        scrollWidth: ref.scrollWidth,
-        height: ref.offsetHeight,
-      });
-    }
-  }, [children]);
+    useEffect(() => {
+      if (ref) {
+        ref.current = scrollableContainerRef.current;
+      }
+    }, [scrollableContainerRef, ref]);
 
-  useEffect(() => {
-    if (dimensions.scrollWidth <= dimensions.width) {
-      setLeftButtonVisibility(false);
-      setRightButtonVisibility(false);
-    }
+    useEffect(() => {
+      const ref = scrollableContainerRef.current;
+      if (ref) {
+        setDimensions({
+          width: ref.offsetWidth,
+          scrollWidth: ref.scrollWidth,
+          height: ref.offsetHeight,
+        });
+      }
+    }, [children]);
 
-    if (dimensions.scrollWidth > dimensions.width) {
-      setRightButtonVisibility(true);
-    }
+    useEffect(() => {
+      if (dimensions.scrollWidth <= dimensions.width) {
+        setLeftButtonVisibility(false);
+        setRightButtonVisibility(false);
+      }
 
-    if (scrollableContainerRef.current) {
-      if (
+      if (dimensions.scrollWidth > dimensions.width) {
+        setRightButtonVisibility(true);
+      }
+
+      if (scrollableContainerRef.current) {
+        if (
+          isEnd(
+            dimensions.width,
+            dimensions.scrollWidth,
+            scrollableContainerRef.current.scrollLeft
+          )
+        ) {
+          setRightButtonVisibility(false);
+        }
+
+        if (scrollableContainerRef.current.scrollLeft > 0) {
+          setLeftButtonVisibility(true);
+        }
+      }
+    }, [dimensions]);
+
+    const handleScroll = (e) => {
+      const { scrollLeft } = e.target;
+      if (scrollLeft > 0) {
+        setLeftButtonVisibility(true);
+      } else {
+        setLeftButtonVisibility(false);
+      }
+
+      if (!isEnd(dimensions.width, dimensions.scrollWidth, scrollLeft)) {
+        setRightButtonVisibility(true);
+      } else {
+        setRightButtonVisibility(false);
+      }
+
+      onScroll instanceof Function && onScroll(e);
+    };
+
+    const scrollLeft = (e) => {
+      if (scrollableContainerRef.current) {
+        scrollableContainerRef.current.scrollBy({
+          left: -dimensions.width,
+          behavior: "smooth",
+        });
+
+        scrollableContainerRef.current.scrollLeft <= 0 &&
+          setLeftButtonVisibility(false);
+      }
+    };
+
+    const scrollRight = (e) => {
+      if (scrollableContainerRef.current) {
+        scrollableContainerRef.current.scrollBy({
+          left: dimensions.width,
+          behavior: "smooth",
+        });
+
         isEnd(
           dimensions.width,
           dimensions.scrollWidth,
           scrollableContainerRef.current.scrollLeft
-        )
-      ) {
-        setRightButtonVisibility(false);
+        ) && setRightButtonVisibility(false);
       }
+    };
 
-      if (scrollableContainerRef.current.scrollLeft > 0) {
-        setLeftButtonVisibility(true);
-      }
-    }
-  }, [dimensions]);
+    return (
+      <ScrollableWrapper>
+        {!hideButtons && (
+          <CustomButtonLeft
+            top={dimensions.height / 2 - buttonSize * 2}
+            $size={buttonSize}
+            color={buttonColor}
+            hoverColor={buttonHoverColor}
+            backgroundColor={buttonBackgroundColor}
+            shadowColor={buttonShadowColor}
+            zIndex={buttonZIndex}
+            visible={leftButtonVisibility && scrollableContainerRef.current}
+            onClick={scrollLeft}
+          >
+            {/* TODO: Icons can be independent with wrapper or else */}
+            <NavigateBefore
+              style={{ height: `${buttonSize}px`, width: `${buttonSize}` }}
+            />
+          </CustomButtonLeft>
+        )}
 
-  const handleScroll = (e) => {
-    const { scrollLeft } = e.target;
-    if (scrollLeft > 0) {
-      setLeftButtonVisibility(true);
-    } else {
-      setLeftButtonVisibility(false);
-    }
+        {!hideButtons && (
+          <CustomButtonRight
+            top={dimensions.height / 2 - buttonSize * 2}
+            $size={buttonSize}
+            color={buttonColor}
+            hoverColor={buttonHoverColor}
+            backgroundColor={buttonBackgroundColor}
+            shadowColor={buttonShadowColor}
+            zIndex={buttonZIndex}
+            visible={rightButtonVisibility && scrollableContainerRef.current}
+            onClick={scrollRight}
+          >
+            <NavigateNext
+              style={{ height: `${buttonSize}px`, width: `${buttonSize}` }}
+            />
+          </CustomButtonRight>
+        )}
 
-    if (!isEnd(dimensions.width, dimensions.scrollWidth, scrollLeft)) {
-      setRightButtonVisibility(true);
-    } else {
-      setRightButtonVisibility(false);
-    }
-
-    onScroll instanceof Function && onScroll(e);
-  };
-
-  const scrollLeft = (e) => {
-    if (scrollableContainerRef.current) {
-      scrollableContainerRef.current.scrollBy({
-        left: -dimensions.width,
-        behavior: "smooth",
-      });
-
-      scrollableContainerRef.current.scrollLeft <= 0 &&
-        setLeftButtonVisibility(false);
-    }
-  };
-
-  const scrollRight = (e) => {
-    if (scrollableContainerRef.current) {
-      scrollableContainerRef.current.scrollBy({
-        left: dimensions.width,
-        behavior: "smooth",
-      });
-
-      isEnd(
-        dimensions.width,
-        dimensions.scrollWidth,
-        scrollableContainerRef.current.scrollLeft
-      ) && setRightButtonVisibility(false);
-    }
-  };
-
-  return (
-    <ScrollableWrapper>
-      {!hideButtons && (
-        <CustomButtonLeft
-          top={dimensions.height / 2 - buttonSize * 2}
-          $size={buttonSize}
-          color={buttonColor}
-          hoverColor={buttonHoverColor}
-          backgroundColor={buttonBackgroundColor}
-          shadowColor={buttonShadowColor}
-          zIndex={buttonZIndex}
-          visible={leftButtonVisibility && scrollableContainerRef.current}
-          onClick={scrollLeft}
+        <ScrollableContainer
+          onScroll={handleScroll}
+          ref={scrollableContainerRef}
+          disableScroll={disableScroll}
         >
-          {/* TODO: Icons can be independent with wrapper or else */}
-          <NavigateBefore
-            style={{ height: `${buttonSize}px`, width: `${buttonSize}` }}
-          />
-        </CustomButtonLeft>
-      )}
-
-      {!hideButtons && (
-        <CustomButtonRight
-          top={dimensions.height / 2 - buttonSize * 2}
-          $size={buttonSize}
-          color={buttonColor}
-          hoverColor={buttonHoverColor}
-          backgroundColor={buttonBackgroundColor}
-          shadowColor={buttonShadowColor}
-          zIndex={buttonZIndex}
-          visible={rightButtonVisibility && scrollableContainerRef.current}
-          onClick={scrollRight}
-        >
-          <NavigateNext
-            style={{ height: `${buttonSize}px`, width: `${buttonSize}` }}
-          />
-        </CustomButtonRight>
-      )}
-
-      <ScrollableContainer
-        onScroll={handleScroll}
-        ref={scrollableContainerRef}
-        disableScroll={disableScroll}
-      >
-        {children}
-      </ScrollableContainer>
-    </ScrollableWrapper>
-  );
-};
+          {children}
+        </ScrollableContainer>
+      </ScrollableWrapper>
+    );
+  }
+);
 
 ScrollableCarousel.defaultProps = {
   buttonSize: 24,
